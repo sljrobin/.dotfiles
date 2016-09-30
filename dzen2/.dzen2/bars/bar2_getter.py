46720f05d81dd971e6813bd787c53638050e615d
sljrobin <sljrobin@gmail.com>
2016-09-30 10:06:28 +0100

    Updated dzen2 bar2

--- dzen2/.dzen2/bars/bar2_getter.py
+++ dzen2/.dzen2/bars/bar2_getter.py
@@ -5,13 +5,14 @@
 #     Description: Bar 2 - Getter
 #          Author: Simon L. J. Robin | https://sljrobin.org
 #         Created: 2016-08-28 23:57:47
-#        Modified: 2016-09-10 18:45:39
+#        Modified: 2016-09-30 10:06:16
 #
 ################################################################################
 import os
 import sys
 sys.path.insert(0, os.environ['HOME'] + "/.dzen2/scripts")
 import Brightness
+import Disk
 import Internet
 
 
@@ -20,8 +21,11 @@ def main():
     """Main function to get information for Bar 2.
     """
     getter_brightness = Brightness.Brightness()
+    getter_disk = Disk.Disk()
     getter_internet = Internet.Internet()
 
+    getter_disk.show_disks()
+    sys.stdout.write(" | ")
     getter_internet.get_internet_device()
     getter_internet.show_wifi()
     sys.stdout.write(" | ")
