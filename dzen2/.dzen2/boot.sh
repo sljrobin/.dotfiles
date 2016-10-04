28a08c33b1b588ef7a575c0eaa4db6138cde449d
sljrobin <sljrobin@gmail.com>
2016-10-04 21:57:55 +0100

    Updated dzen2 boot

---
+++ dzen2/.dzen2/boot.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+#        Filename: boot.sh
+#     Description: Start all the Dzen bars
+#          Author: Simon L. J. Robin | https://sljrobin.org
+#         Created: 2016-06-28 18:38:08
+#        Modified: 2016-06-28 18:51:32
+#
+################################################################################
+$HOME/.dzen2/bars/bar2_starter.sh&
+$HOME/.dzen2/bars/bar3_starter.sh&
+$HOME/.dzen2/bars/bar4_starter.sh&
