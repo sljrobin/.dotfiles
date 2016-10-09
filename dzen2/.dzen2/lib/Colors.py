0ef1d805fbeed3f07666408d098381f632646bce
sljrobin <sljrobin@gmail.com>
2016-10-09 21:25:24 +0100

    Added dzen2 lib Colors

---
+++ dzen2/.dzen2/lib/Colors.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#        Filename: Colors.py
+#     Description: Functions for colors
+#          Author: Simon L. J. Robin | https://sljrobin.org
+#         Created: 2016-08-28 16:27:45
+#        Modified: 2016-08-28 22:47:39
+#
+################################################################################
+CL_BASE00 = "#181818"  # Gray 1
+CL_BASE01 = "#282828"  # Gray 2
+CL_BASE02 = "#383838"  # Gray 3
+CL_BASE03 = "#585858"  # Gray 4
+CL_BASE04 = "#B8B8B8"  # Gray 5
+CL_BASE05 = "#D8D8D8"  # White 1
+CL_BASE06 = "#E8E8E8"  # White 2
+CL_BASE07 = "#F8F8F8"  # White 3
+CL_BASE08 = "#AB4642"  # Red
+CL_BASE09 = "#DC9656"  # Orange
+CL_BASE0A = "#F7CA88"  # Yellow
+CL_BASE0B = "#A1B56C"  # Green
+CL_BASE0C = "#86C1B9"  # Cyan
+CL_BASE0D = "#7CAFC2"  # Blue
+CL_BASE0E = "#BA8BAF"  # Purple
+CL_BASE0F = "#A16946"  # Brown
